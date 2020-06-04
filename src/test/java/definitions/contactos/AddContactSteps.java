70dbe482a017d8fb0be1e6b47dcc14009e193488
Eliseo Castro <ext_eecastrov@falabella.cl>
2020-06-03 23:02:58 -0400

    Implementacion de Driver Factory

--- src/test/java/definitions/contactos/AddContactSteps.java
+++ src/test/java/definitions/contactos/AddContactSteps.java
@@ -1,49 +1,74 @@
 package definitions.contactos;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.DriverFactory;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AddContactSteps {
 
     private AppiumDriver driver;
+    private Wait wait;
+
+    @Before
+    public void setup() throws IOException {
+        abrirAppMobile();
+    }
+
+    private void abrirAppMobile() throws IOException {
+        System.out.println("Open app on mobile device");
+        driver = DriverFactory.createMobileDriver();
+        driver.launchApp();
+    }
 
     @Given("^un usuario se encuentra loggeado$")
-    public void unUsuarioSeEncuentraLoggeado() throws MalformedURLException {
+    public void unUsuarioSeEncuentraLoggeado() {
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("platformName", "android");
-        caps.setCapability("deviceName", "SAMSUNG_S8_QA_1");
-        caps.setCapability("appPackage", "com.truecaller");
-        caps.setCapability("appActivity", "com.truecaller.ui.TruecallerInit");
+        wait = new WebDriverWait(driver, 30);
 
-        driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("com.truecaller:id/nextButton")));
         MobileElement el1 = (MobileElement) driver.findElementById("com.truecaller:id/nextButton");
         el1.click();
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/button1")));
         MobileElement el2 = (MobileElement) driver.findElementById("android:id/button1");
         el2.click();
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("com.truecaller:id/button_skip")));
         MobileElement el3 = (MobileElement) driver.findElementById("com.truecaller:id/button_skip");
         el3.click();
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageButton")));
         MobileElement el4 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageButton");
         el4.click();
 
     }
 
     @Given("^un usuario se encuentra en contactos$")
-    public void unUsuarioSeEncuentraEnContactos() throws InterruptedException, MalformedURLException {
+    public void unUsuarioSeEncuentraEnContactos() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("Contactos")));
         MobileElement el30 = (MobileElement) driver.findElementByAccessibilityId("Contactos");
         el30.click();
     }
@@ -51,8 +76,11 @@ public class AddContactSteps {
 
     @When("^el usuario agrega un contacto$")
     public void elUsuarioAgregaUnContacto() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("com.truecaller:id/fab_icon")));
         MobileElement el40 = (MobileElement) driver.findElementById("com.truecaller:id/fab_icon");
         el40.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.samsung.android.contacts:id/nameEdit")));
         MobileElement el5 = (MobileElement) driver.findElementById("com.samsung.android.contacts:id/nameEdit");
         el5.sendKeys("AutomatizacionMobile");
         driver.navigate().back();
@@ -66,6 +94,33 @@ public class AddContactSteps {
 
     @Then("^el contacto se visualiza en la lista de contactos$")
     public void elContactoSeVisualizaEnLaListaDeContactos() {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//android.widget.TextView[@resource-id='com.truecaller:id/title']")));
+        while(getContact("AutomatizacionMobile").size() == 0) {
+            scrollDown();
+        }
+        if(getContact("AutomatizacionMobile").size() > 0){
+            System.out.println("Contacto encontrado: ["+ getContact("AutomatizacionMobile").get(0).getText() +"]");
+            Assert.assertEquals("AutomatizacionMobile", getContact("AutomatizacionMobile").get(0).getText());
+        }else
+            System.out.println("No se encontro el contacto que debio haber sido creado.");
+        driver.closeApp();
+    }
 
+    private List<WebElement> getContact(String nombreContacto){
+        return driver.findElementsByXPath("//android.widget.TextView[@text='"+ nombreContacto +"']");
+    }
+
+    private void scrollDown(){
+        System.out.println("Scroll Down");
+        Dimension dimension = driver.manage().window().getSize();
+        Double scrollHeightStart = dimension.getHeight() * 0.5;
+        int scrollStart = scrollHeightStart.intValue();
+        Double scrollHeightEnd = dimension.getHeight() * 0.2;
+        int scrollEnd = scrollHeightEnd.intValue();
+        new TouchAction<>((PerformsTouchActions) driver)
+                .press(PointOption.point(0, scrollStart))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                .moveTo(PointOption.point(0, scrollEnd))
+                .release().perform();
     }
 }
---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,83 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private static AppiumDriver driverMobile;
+
+    private static DesiredCapabilities readCapabilitiesFromProperties(Properties properties){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        for(Object key : properties.keySet())
+            capabilities.setCapability(key.toString(), properties.getProperty(key.toString()));
+        return capabilities;
+    }
+
+    private static AppiumDriver initializeMobileDriver(String urlAppium, Properties properties) throws MalformedURLException {
+        DesiredCapabilities capsMobile = readCapabilitiesFromProperties(properties);
+        System.out.println("Inicializa el appium driver apuntando a la URL de appium: ["+ urlAppium +"]");
+        driverMobile = new AppiumDriver(new URL(urlAppium), capsMobile);
+        //driverMobile.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driverMobile;
+    }
+
+    /**
+     * Recibe la URL de Appium y el archivo properties con las capabilities a usar desde parametros pasados al metodo
+     */
+    public static AppiumDriver createMobileDriver(String urlAppium, Properties capabilities) throws MalformedURLException {
+        return initializeMobileDriver(urlAppium, capabilities);
+    }
+
+    /**
+     * Obtiene la URL de Appium y el nombre del archivo properties con las capabilities a usar desde parametros pasados
+     * en la linea de comandos como se ve en el siguiente ejemplo:
+     * mvn clean test -DurlAppium="http://0.0.0.0:4725/wd/hub" -DpropertiesFileName="SAMSUNG_S8_QA_1"
+     */
+    public static AppiumDriver createMobileDriver() throws IOException {
+        HashMap params = getParamsFromCommandLine();
+        Properties properties = readPropertiesFromFile(params.get("propertiesFileName").toString());
+        return initializeMobileDriver(params.get("urlAppium").toString(), properties);
+    }
+
+    private static HashMap getParamsFromCommandLine(){ //inline condition
+        String urlAppium           = (System.getProperty("urlAppium") == null)? "http://0.0.0.0:4723/wd/hub" : System.getProperty("urlAppium");
+        String propertiesFileName  = (System.getProperty("propertiesFileName") == null)? "default" : System.getProperty("propertiesFileName");
+        HashMap params = new HashMap<>();
+        params.put("urlAppium", urlAppium);
+        params.put("propertiesFileName", propertiesFileName);
+        return params;
+    }
+
+    private static Properties readPropertiesFromFile(String propertiesFileName) throws IOException {
+        System.out.println("Read Capabilities From Properties File: " + propertiesFileName + ".properties");
+        File propFileName = new File("src/test/resources/properties/"+ propertiesFileName +".properties").getAbsoluteFile();
+        FileInputStream inputStream = new FileInputStream(propFileName);
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        return properties;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
